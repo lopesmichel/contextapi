73554da051154ec379b67adcfc1959c202e7a023
Michel Lopes <lopesmichel.lp@gmail.com>
2025-04-15 22:47:13 -0300

    configurando rota

--- context-api/src/App.jsx
+++ context-api/src/App.jsx
@@ -1,14 +1,21 @@
 import {  } from 'react'
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './App.css'
+
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <div>
       <h1>context</h1>
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App
